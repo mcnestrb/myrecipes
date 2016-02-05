9fdd2c42a4f6e5a50ec60315b031e335718b2aa0
mcnestrb <mcnestrb@tcd.ie>
2016-02-05 02:00:07 +0000

    create chefs and recipes tables

---
+++ db/migrate/20160205013801_creat_recipes.rb
@@ -0,0 +1,10 @@
+class CreatRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :summary
+      t.text :description
+      t.timestamps
+    end
+  end
+end
